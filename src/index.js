4d225f89ab0408c4f9746ac63d04ac9778088110
Raul <raulhernandezguerrero@gmail.com>
2022-08-12 20:46:46 -0400

    Primer Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import { saludar } from './js/componentes';
+import './styles.css';
+
+const nombre = 'Raúl';
+
+saludar ( nombre );
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebPack    = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin     = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+
+    output: {
+        clean: true 
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false, 
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /style.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /style.css$/,
+                use: [ MiniCssExtract.loader, 'css-loader' ] 
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    optimization: {},
+
+    plugins: [
+        new HtmlWebPack({
+            title: 'Mi Webpack App',
+            template: './src/index.html', 
+            filename: './index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: 'nuevo-estilo.css',
+            ignoreOrder: false 
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/assets/', to: 'assets/'
+                }
+            ]
+        })
+    ],
+} 
